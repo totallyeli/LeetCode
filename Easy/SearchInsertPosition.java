50001e4df8f5d9b7a3843e18674666f0425a3115
Elias Bugiel <bugiel794@gmail.com>
2023-12-06 16:58:01 +0100

    finished SearchInsertPosition.java

---
+++ Easy/SearchInsertPosition.java
@@ -0,0 +1,9 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        int i = 0;
+        while (i < nums.length && nums[i] < target) {
+            i++;
+        }
+        return i;
+    }
+}
