c7e50b7e2e50bcda84b201cc0ba5f05028a46270
Elias Bugiel <bugiel794@gmail.com>
2023-12-22 13:56:31 +0100

    finished SetMismatch.java

---
+++ Easy/SetMismatch.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+public class SetMismatch {
+    public int[] findErrorNums(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int dup = -1, missing = -1;
+        for (int i = 1; i <= nums.length; i++) {
+            if (map.containsKey(i)) {
+                if (map.get(i) == 2) {
+                    dup = i;
+                }
+            } else {
+                missing = i;
+            }
+        }
+
+        return new int[]{dup, missing};
+    }
+}
