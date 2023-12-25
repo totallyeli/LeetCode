ec36933c55abf2a653d0549c57812cb3e7b3be4e
Elias Bugiel <bugiel794@gmail.com>
2023-12-25 19:50:41 +0100

    finished FindTheIndexofTheFirstOccurrenceinaString.java

---
+++ Easy/FindTheIndexofTheFirstOccurrenceinaString.java
@@ -0,0 +1,13 @@
+public class FindTheIndexofTheFirstOccurrenceinaString {
+    public int strStr(String haystack, String needle) {
+        if (needle.isEmpty()) {
+            return 0;
+        }
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            if (haystack.substring(i, i + needle.length()).equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
